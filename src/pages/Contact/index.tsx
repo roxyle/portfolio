cbfc61ce50e6d2af4af26f3e22ec3b5059d1c051
roxyle <ylenia.rossi@outlook.com>
2025-09-26 15:04:26 +0200

    edited onClick on contact-mail

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -12,14 +12,19 @@ const Contact = () => {
         <div className='max-w-3xl border border-x-transparent border-y-pink-500 p-5'>
         <p className='py-4 text-center'> 
         If I have captured your attention or if you'd like to have a chat, you can contact me here:</p>
+      
+        <div className='leading-relaxed text-base'>          
+          <p className='flex justify-center text-white'>
+            <strong>
+              <a href="mailto:ylenia.rossi@outlook.com" className='underline hover:text-pink-400'>
+                ylenia.rossi@outlook.com
+              </a>
+            </strong>
+          </p>
+        </div>
       </div>
 
-      
-        <div className='py-4'>          
-          <p className='flex justify-center text-white'><strong>ylenia.rossi@outlook.com</strong></p>
-        </div>
-
-        <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center justify-center pt-6'>
           <Image src='/teamwork.gif' alt='help me help you' width={300} height={300} />
         </div>
       </div>
